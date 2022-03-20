f24e8d67697f698b6ac56f5b9c2ed0f0c05a1033
Gitea <SStoyanov22@gmail.com>
2022-03-20 08:55:41 +0200

    Adding Contact Handler

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/SStoyanov22/lenslocked
 
 go 1.15
-
-require github.com/cortesi/modd v0.0.0-20211215124449-6083f9d1c171 // indirect
--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awsome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:sstoyanov22@gmail.com\">sstoyanov22@gmail.com</a>.</p>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }

cef302e9f9ba23459b20ddd89a9359f1de0b33fb
Gitea <SStoyanov22@gmail.com>
2022-03-20 11:57:24 +0200

    Implementing the Handler interface; Creating router struct that implements it; pass the router as parameter to ListenAndServe

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+const (
+	//...
+	statusNotFound = 404
+	//...
+)
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awsome site!</h1>")
@@ -15,21 +21,25 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:sstoyanov22@gmail.com\">sstoyanov22@gmail.com</a>.</p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "<h1>Page not Found</h1>", http.StatusNotFound)
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		//TODO
+		errorHandler(w, r)
 	}
-	fmt.Fprint(w, r.URL.Path)
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }

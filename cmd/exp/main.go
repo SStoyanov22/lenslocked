ae36c228d238e0f82b7f77e6c5bd7454bf3a1d1f
Gitea <SStoyanov22@gmail.com>
2022-04-02 14:20:03 +0300

    Some experiments - accessing a map through template

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 )
 
 type User struct {
+	Name  string
+	Bio   string
+	Role  Role
+	Items map[string]string
+}
+
+type Role struct {
 	Name string
-	Bio  string
 }
 
 func main() {
@@ -16,11 +23,21 @@ func main() {
 		panic(err)
 	}
 
+	role := Role{
+		Name: "Admin",
+	}
+	items := map[string]string{}
+
+	items["key1"] = "value1"
+	items["key2"] = "value2"
 	user := User{
-		Name: "Stoyan Stoyanov",
-		Bio:  `<script>alert("Haha, you have been hacked!")</script>`,
+		Name:  "Stoyan Stoyanov",
+		Bio:   `<script>alert("Haha, you have been hacked!")</script>`,
+		Role:  role,
+		Items: items,
 	}
 
+	fmt.Println(items["key1"])
 	err = t.Execute(os.Stdout, user)
 
 	if err != nil {
